d58ec70ef5a4c3d4c6e26b8dda6def694e51b02c
Pankaj Pant <44195004+pankaj-pant@users.noreply.github.com>
2026-03-10 14:44:24 +0200

    add 1.9.11 tag to yml

--- packages/gateway/src/features/search/root-resolvers.ts
+++ packages/gateway/src/features/search/root-resolvers.ts
@@ -198,22 +198,6 @@ export const resolvers: GQLResolver = {
           console.log('postMetrics took', performance.now() - start, 'ms')
         }
 
-        console.log(
-          'searchEvents returning  :>> ',
-          JSON.stringify({
-            totalItems:
-              (searchResult &&
-                searchResult.body.hits &&
-                searchResult.body.hits.total.value) ||
-              0,
-            results:
-              (searchResult &&
-                searchResult.body.hits &&
-                searchResult.body.hits.hits) ||
-              []
-          })
-        )
-
         return {
           totalItems:
             (searchResult &&
