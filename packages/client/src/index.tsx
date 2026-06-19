e4fd8ca0281c525c28dd6d853613434bc1eb2269
solomcfly <solo.ramilison27@gmail.com>
2026-06-19 14:39:56 +0300

    fix

--- packages/client/src/index.tsx
+++ packages/client/src/index.tsx
@@ -72,12 +72,13 @@ const router = createBrowserRouter(routesConfig, {
   }
 })
 
-// eslint-disable-next-line no-console
-console.debug('Waiting for service worker to be ready...')
-void navigator.serviceWorker.ready.then(() => {
+root.render(<App router={router} store={store} />)
+
+if ('serviceWorker' in navigator) {
   // eslint-disable-next-line no-console
-  console.debug('Service worker is ready')
-  root.render(<App router={router} store={store} />)
-})
+  void navigator.serviceWorker.ready.then(() =>
+    console.debug('Service worker is ready')
+  )
+}
 
 new SubmissionController(store).start()
